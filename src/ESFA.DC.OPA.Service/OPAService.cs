3d1f617f0fc9806850f17e4984d793257d6d1849
Mike Childs <mike@mjcdev.co.uk>
2018-06-14 13:20:40 +0100

    Initial Commit of OPA.Service

---
+++ src/ESFA.DC.OPA.Service/OPAService.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Reflection;
+using ESFA.DC.OPA.Model.Interface;
+using ESFA.DC.OPA.Service.Interface;
+using ESFA.DC.OPA.Service.Interface.Builders;
+using ESFA.DC.OPA.Service.Interface.Rulebase;
+using Oracle.Determinations.Engine;
+
+namespace ESFA.DC.OPA.Service
+{
+    public class OPAService : IOPAService
+    {
+        private readonly ISessionBuilder _sessionBuilder;
+        private readonly IOPADataEntityBuilder _dataEntityBuilder;
+        private readonly IRulebaseProviderFactory _rulebaseProviderFactory;
+
+        public OPAService(ISessionBuilder sessionBuilder, IOPADataEntityBuilder dataEntityBuilder, IRulebaseProviderFactory rulebaseProviderFactory)
+        {
+            _sessionBuilder = sessionBuilder;
+            _dataEntityBuilder = dataEntityBuilder;
+            _rulebaseProviderFactory = rulebaseProviderFactory;
+        }
+
+        public IDataEntity ExecuteSession(IDataEntity globalEntity)
+        {
+            var rulebaseProvider = _rulebaseProviderFactory.Build();
+
+            Session session;
+
+            using (Stream stream = rulebaseProvider.GetStream(Assembly.GetCallingAssembly()))
+            {
+                session = _sessionBuilder.CreateOPASession(stream, globalEntity);
+            }
+
+            session.Think();
+
+            var outputGlobalInstance = session.GetGlobalEntityInstance();
+            var outputEntity = _dataEntityBuilder.CreateOPADataEntity(outputGlobalInstance, null);
+
+            return outputEntity;
+        }
+    }
+}
